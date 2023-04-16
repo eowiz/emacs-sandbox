53b528a91da1729b466d1f28fb28facad6c39019
eowiz <eowiz@proton.me>
2023-04-17 00:27:50 +0900

    init commit

---
+++ emacs-sandbox.el
@@ -0,0 +1,39 @@
+;;; emacs-sandbox.el ---   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  eowiz
+
+;; Author: eowiz <eowiz@proton.me>
+;; Maintainer: eowiz <eowiz@proton.me>
+;; URL: 
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "29.0"))
+;; Keywords: 
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; 
+;;
+
+;;; Code:
+
+;; Happy coding! ;)
+
+(message "Here is sandbox")
+
+(provide 'emacs-sandbox)
+;;; emacs-sandbox.el ends here
